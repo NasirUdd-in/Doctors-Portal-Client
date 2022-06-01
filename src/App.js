950521a4451022a71f419a9b4ba7e7d1e7eb86e7
Nasir Uddin <nasir.diudevs@gmail.com>
2022-06-01 07:47:32 +0600

    up

--- src/App.js
+++ src/App.js
@@ -6,24 +6,10 @@ import Home from "./Pages/Home/Home/Home";
 import Appointment from "./Pages/Appointment/Appointment/Appointment";
 import Login from "./Pages/Login/Login";
 
-
 function App() {
   return (
     <div className="App">
       <Router>
-
-        <Switch>
-          <Route path="/appointment">
-            <Appointment />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-        </Switch> */}
-
         <Switch>
           <Switch>
             <Route path="/appointment">
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,16 +1,71 @@
 import React from "react";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import login from "../../../images/login.png";
+import { Grid } from "@mui/material";
+import {
+  Container,
+  Typography,
+  TextField,
+  Button,
+  CircularProgress,
+  Alert,
+} from "@mui/material";
+import { NavLink, useLocation, useHistory } from "react-router-dom";
+import login from "../../images/login.png";
 
 const Login = () => {
+  const handleOnChange = (e) => {};
+  const handleLoginSubmit = (e) => {};
+
+  const handleGoogleSignIn = () => {};
   return (
     <div>
       <Container>
         <Grid container spacing={2}>
-          <Grid item xs={12} md={6}></Grid>
           <Grid item xs={12} md={6}>
-            {/* <img style={{ width: "100%" }} src={login} alt="" /> */}
+            <Typography variant="body1" gutterBottom>
+              Login
+            </Typography>
+            <form onSubmit={handleLoginSubmit}>
+              <TextField
+                sx={{ width: "75%", m: 1 }}
+                id="standard-basic"
+                label="Your Email"
+                name="email"
+                onChange={handleOnChange}
+                variant="standard"
+              />
+              <TextField
+                sx={{ width: "75%", m: 1 }}
+                id="standard-basic"
+                label="Your Password"
+                type="password"
+                name="password"
+                onChange={handleOnChange}
+                variant="standard"
+              />
+
+              <Button
+                sx={{ width: "75%", m: 1 }}
+                type="submit"
+                variant="contained"
+              >
+                Login
+              </Button>
+              <NavLink style={{ textDecoration: "none" }} to="/register">
+                <Button variant="text">New User? Please Register</Button>
+              </NavLink>
+              {/* {isLoading && <CircularProgress />}
+              {user?.email && (
+                <Alert severity="success">Login successfully!</Alert>
+              )}
+              {authError && <Alert severity="error">{authError}</Alert>} */}
+            </form>
+            <p>------------------------</p>
+            <Button onClick={handleGoogleSignIn} variant="contained">
+              Google Sign In
+            </Button>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <img style={{ width: "100%" }} src={login} alt="" />
           </Grid>
         </Grid>
       </Container>
